8dbe5b652d244350ab383eb0065ad39461a1018d
Tom Ainsworth <tomainsworth94@gmail.com>
2023-03-06 17:32:35 +0000

    Refactor remaining class components
    Remove commented class code
    Refactor Card, CardList and SearchBox to functional components

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,28 +1,12 @@
-import { Component } from "react";
 import Card from "../card/card.component";
 import "./card-list.styles.css";
 
-class CardList extends Component {
-	render() {
-		// console.log(this.props);
-		// console.log("Render CardList");
-		const { monsters } = this.props;
-		return (
-			<div className="card-list">
-				{monsters.map((monster) => {
-					const { name, id, email } = monster;
-					return (
-						<Card
-							key={id}
-							alt={name}
-							src={`https://robohash.org/${id}?set=set2&size=180x180`}
-							email={email}
-						/>
-					);
-				})}
-			</div>
-		);
-	}
-}
+const CardList = ({ monsters }) => (
+	<div className="card-list">
+		{monsters.map((monster) => {
+			return <Card key={monster.id} monster={monster} />;
+		})}
+	</div>
+);
 
 export default CardList;
